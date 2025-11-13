76ae20eaf3eac0585d1619237ecf352a55e2f6db
Adrien Vernier <177334560+adrievx@users.noreply.github.com>
2025-11-13 22:10:00 +0000

    [Utils] Added Base64 and StringUtils

---
+++ Utils/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adr.GlobalUtils.Utils {
+    public class StringUtils {
+        private static Random _random = new Random();
+
+        public static string RandomUpperString(int length) {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
